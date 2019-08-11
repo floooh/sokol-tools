9ec005f8bfd2a81849d9ba425a9e3e8351689149
Mattias Ljungstrom <mattias.ljungstrom@gmail.com>
2019-08-11 21:02:35 +0200

    macOS compilation fixes

--- src/shdc/shdc.h
+++ src/shdc/shdc.h
@@ -215,6 +215,9 @@ struct line_t {
     std::string line;       // line content
     int filename = 0;       // index into input_t filenames
     int index = 0;          // line index == line nr - 1
+
+    line_t() { };
+    line_t(const std::string& ln, int fn, int ix): line(ln), filename(fn), index(ix) { };
 };
 
 /* pre-parsed GLSL source file, with content split into snippets */
