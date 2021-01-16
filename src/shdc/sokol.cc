3bc3114fa840c29381a1c41eaffc85b6996ae599
Andre Weissflog <floooh@gmail.com>
2021-01-16 19:10:49 +0100

    change SG_RANGE_REF() to &SG_RANGE() in the 'usage doc header'

--- src/shdc/sokol.cc
+++ src/shdc/sokol.cc
@@ -155,7 +155,7 @@ static void write_header(const args_t& args, const input_t& inp, const spirvcros
             L("            .{} = ...;\n", uniform.name);
         };
         L("        }};\n");
-        L("        sg_apply_uniforms(SG_SHADERSTAGE_[VS|FS], SLOT_{}{}, SG_RANGE_REF({}));\n", mod_prefix(inp), ub.name, ub.name);
+        L("        sg_apply_uniforms(SG_SHADERSTAGE_[VS|FS], SLOT_{}{}, &SG_RANGE({}));\n", mod_prefix(inp), ub.name, ub.name);
         L("\n");
     }
     L("*/\n");
