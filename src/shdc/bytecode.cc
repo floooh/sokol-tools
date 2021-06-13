744c924ce2b4ca18a72a6ad09c572cafda733b49
Josh Soref <jsoref@users.noreply.github.com>
2021-06-13 17:36:34 -0400

    spelling: command line
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/shdc/bytecode.cc
+++ src/shdc/bytecode.cc
@@ -3,7 +3,7 @@
     when running on Windos, Metal only works when running on macOS.
 
     Uses d3dcompiler.dll for HLSL, and for Metal, invokes the Metal
-    compiler toolchain commandline tools.
+    compiler toolchain command line tools.
 
     On Metal, bytecode compilation only happens for the macOS and iOS
     targets, but not for running in the simulator, in this case,
