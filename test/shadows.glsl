45b15b71d7fba29135d82b2a5a5562f6987b3e52
Andre Weissflog <floooh@gmail.com>
2022-03-02 19:37:06 +0100

    add shadows.glsl test shader (shadow mapping)

---
+++ test/shadows.glsl
@@ -0,0 +1,165 @@
+//------------------------------------------------------------------------------
+//  float/rgba8 encoding/decoding so that we can use an RGBA8
+//  shadow map instead of floating point render targets which might
+//  not be supported everywhere
+//
+//  http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
+//
+@ctype mat4 hmm_mat4
+@ctype vec3 hmm_vec3
+@ctype vec2 hmm_vec2
+
+@block util
+
+float decodeDepth(vec4 rgba) {
+    return dot(rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/16581375.0));
+}
+
+//------------------------------------------------------------------------------
+//  perform simple shadow map lookup returns 0.0 (unlit) or 1.0 (lit)
+//
+float sampleShadow(vec2 uv, float compare) {
+    #if !SOKOL_GLSL
+    uv.y = 1.0-uv.y;
+    #endif
+    float depth = decodeDepth(texture(shadowMap, vec2(uv.x, uv.y)));
+    depth += 0.001;
+    return step(compare, depth);
+}
+
+//------------------------------------------------------------------------------
+//  perform percentage-closer shadow map lookup
+//
+float sampleShadowPCF(vec2 uv, vec2 smSize, float compare) {
+    float result = 0.0;
+    for (int x=-2; x<=2; x++) {
+        for (int y=-2; y<=2; y++) {
+            vec2 off = vec2(x,y)/smSize;
+            result += sampleShadow(uv+off, compare);
+        }
+    }
+    return result / 25.0;
+}
+
+//------------------------------------------------------------------------------
+//  perform gamma correction
+//
+vec4 gamma(vec4 c) {
+    float p = 1.0/2.2;
+    return vec4(pow(c.xyz, vec3(p, p, p)), c.w);
+}
+@end
+
+//------------------------------------------------------------------------------
+//  Shadowmap pass shaders
+//
+@vs shadowVS
+
+uniform vs_shadow_params {
+    mat4 mvp;
+};
+
+in vec4 position;
+out vec2 projZW;
+
+void main() {
+    gl_Position = mvp * position;
+    projZW = gl_Position.zw;
+}
+@end
+
+@fs shadowFS
+in vec2 projZW;
+out vec4 fragColor;
+
+vec4 encodeDepth(float v) {
+    vec4 enc = vec4(1.0, 255.0, 65025.0, 16581375.0) * v;
+    enc = fract(enc);
+    enc -= enc.yzww * vec4(1.0/255.0,1.0/255.0,1.0/255.0,0.0);
+    return enc;
+}
+
+void main() {
+    float depth = projZW.x / projZW.y;
+    fragColor = encodeDepth(depth);
+}
+@end
+
+@program shadow shadowVS shadowFS
+
+//------------------------------------------------------------------------------
+//  Color pass shaders
+//
+@vs colorVS
+uniform vs_light_params {
+    mat4 model;
+    mat4 mvp;
+    mat4 lightMVP;
+    vec3 diffColor;
+};
+
+in vec4 position;
+in vec3 normal;
+out vec3 color;
+out vec4 lightProjPos;
+out vec4 P;
+out vec3 N;
+
+void main() {
+    gl_Position = mvp * position;
+    lightProjPos = lightMVP * position;
+    P = (model * position);
+    N = (model * vec4(normal, 0.0)).xyz;
+    color = diffColor;
+}
+@end
+
+@fs colorFS
+
+@texture 2D shadowMap
+@include_block util
+
+uniform fs_light_params {
+    vec2 shadowMapSize;
+    vec3 lightDir;
+    vec3 eyePos;
+};
+
+in vec3 color;
+in vec4 lightProjPos;
+in vec4 P;
+in vec3 N;
+out vec4 fragColor;
+
+void main() {
+    float specPower = 2.2;
+    float ambientIntensity = 0.25;
+
+    // diffuse lighting
+    vec3 l = normalize(lightDir);
+    vec3 n = normalize(N);
+    float n_dot_l = dot(n,l);
+    if (n_dot_l > 0.0) {
+
+        vec3 lightPos = lightProjPos.xyz / lightProjPos.w;
+        vec2 smUV = (lightPos.xy+1.0)*0.5;
+        float depth = lightPos.z;
+        float s = sampleShadowPCF(smUV, shadowMapSize, depth);
+
+        float diffIntensity = max(n_dot_l * s, 0.0);
+
+        vec3 v = normalize(eyePos - P.xyz);
+        vec3 r = reflect(-l, n);
+        float r_dot_v = max(dot(r, v), 0.0);
+        float specIntensity = pow(r_dot_v, specPower) * n_dot_l * s;
+
+        fragColor = vec4(vec3(specIntensity, specIntensity, specIntensity) + (diffIntensity+ambientIntensity)*color, 1.0);
+    }
+    else {
+        fragColor = vec4(color * ambientIntensity, 1.0);
+    }
+    fragColor = gamma(fragColor);
+}
+@end
+
+@program color colorVS colorFS
