0e7590abfc95f0c0f10427783b87cdfb114c57ca
Andre Weissflog <floooh@gmail.com>
2023-05-22 14:32:17 +0200

    bump ios-metal version to 1.1 for binary output (1.0 is deprecated and doesn't have fma() yet

--- src/shdc/bytecode.cc
+++ src/shdc/bytecode.cc
@@ -160,7 +160,7 @@ static bool mtl_cc(const std::string& src_path, const std::string& out_dia, cons
         cmdline += " -mmacosx-version-min=10.11 -std=osx-metal1.1 ";
     }
     else {
-        cmdline += " -miphoneos-version-min=9.0 -std=ios-metal1.0 ";
+        cmdline += " -miphoneos-version-min=9.0 -std=ios-metal1.1 ";
     }
     cmdline += src_path;
     return 0 == xcrun(cmdline, output, slang);
@@ -392,4 +392,3 @@ void bytecode_t::dump_debug() const {
 }
 
 } // namespace shdc
-
