82de1b7e806319fbb102f92c938f9a05372a00e8
Andre Weissflog <floooh@gmail.com>
2024-03-27 18:51:23 +0100

    convert Bare generator to new style

--- src/shdc/generators/bare.cc
+++ src/shdc/generators/bare.cc
@@ -9,15 +9,12 @@
 
 namespace shdc::gen {
 
+using namespace refl;
 using namespace util;
 
-static ErrMsg write_stage(const std::string& file_path,
-                          const SpirvcrossSource* src,
-                          const BytecodeBlob* blob)
-{
-    // write text or binary to output file
+static ErrMsg write_file(const std::string& file_path, const SpirvcrossSource* src, const BytecodeBlob* blob) {
     FILE* f = fopen(file_path.c_str(), "wb");
-    if (!f) {
+    if (f == nullptr) {
         return ErrMsg::error(file_path, 0, fmt::format("failed to open output file '{}'", file_path));
     }
     const void* write_data;
@@ -32,64 +29,46 @@ static ErrMsg write_stage(const std::string& file_path,
     }
     size_t written = fwrite(write_data, 1, write_count, f);
     if (written != write_count) {
+        fclose(f);
         return ErrMsg::error(file_path, 0, fmt::format("failed to write output file '{}'", file_path));
     }
     fclose(f);
     return ErrMsg();
 }
 
-static ErrMsg write_shader_sources_and_blobs(const Args& args,
-                                               const Input& inp,
-                                               const Spirvcross& spirvcross,
-                                               const Bytecode& bytecode,
-                                               Slang::Enum slang)
-{
-    for (const auto& item: inp.programs) {
-        const Program& prog = item.second;
-        const SpirvcrossSource* vs_src = find_spirvcross_source_by_shader_name(prog.vs_name, inp, spirvcross);
-        const SpirvcrossSource* fs_src = find_spirvcross_source_by_shader_name(prog.fs_name, inp, spirvcross);
-        const BytecodeBlob* vs_blob = find_bytecode_blob_by_shader_name(prog.vs_name, inp, bytecode);
-        const BytecodeBlob* fs_blob = find_bytecode_blob_by_shader_name(prog.fs_name, inp, bytecode);
-
-        const std::string file_path_base = fmt::format("{}_{}{}_{}", args.output, mod_prefix(inp), prog.name, Slang::to_str(slang));
-        const std::string file_path_vs = fmt::format("{}_vs{}", file_path_base, util::slang_file_extension(slang, vs_blob != nullptr));
-        const std::string file_path_fs = fmt::format("{}_fs{}", file_path_base, util::slang_file_extension(slang, fs_blob != nullptr));
-
-        ErrMsg err;
-        err = write_stage(file_path_vs, vs_src, vs_blob);
-        if (err.valid()) {
-            return err;
-        }
-        err = write_stage(file_path_fs, fs_src, fs_blob);
-        if (err.valid()) {
-            return err;
-        }
-    }
-
-    return ErrMsg();
-}
-
-static ErrMsg _generate(const GenInput& gen) {
-    for (int i = 0; i < Slang::Num; i++) {
-        Slang::Enum slang = (Slang::Enum) i;
-        if (gen.args.slang & Slang::bit(slang)) {
-            ErrMsg err = check_errors(gen.inp, gen.spirvcross[i], slang);
-            if (err.valid()) {
-                return err;
-            }
-            err = write_shader_sources_and_blobs(gen.args, gen.inp, gen.spirvcross[i], gen.bytecode[i], slang);
-            if (err.valid()) {
-                return err;
-            }
-        }
-    }
-
-    return ErrMsg();
-}
-
-//------------------------------------------------------------------------------
+// completely override the generate function since there's no overlap with code-generators
 ErrMsg BareGenerator::generate(const GenInput& gen) {
-    return _generate(gen);
+    const std::string mod_prefix = gen.inp.module.empty() ? "" : fmt::format("{}_", gen.inp.module);
+    ErrMsg err = check_errors(gen);
+    if (err.valid()) {
+        return err;
+    }
+    for (int i = 0; i < Slang::Num; i++) {
+        Slang::Enum slang = Slang::from_index(i);
+        if (gen.args.slang & Slang::bit(slang)) {
+            const Spirvcross& spirvcross = gen.spirvcross[slang];
+            const Bytecode& bytecode = gen.bytecode[slang];
+            for (const ProgramReflection& prog: gen.refl.progs) {
+                for (int stage_index = 0; stage_index < ShaderStage::Num; stage_index++) {
+                    const StageReflection& refl = prog.stages[stage_index];
+                    const SpirvcrossSource* src = spirvcross.find_source_by_snippet_index(refl.snippet_index);
+                    const BytecodeBlob* blob = bytecode.find_blob_by_snippet_index(refl.snippet_index);
+                    const std::string file_path = fmt::format("{}_{}{}_{}_{}{}",
+                        gen.args.output,
+                        mod_prefix,
+                        prog.name,
+                        Slang::to_str(slang),
+                        pystring::lower(refl.stage_name),
+                        slang_file_extension(slang, blob != nullptr));
+                    err = write_file(file_path, src, blob);
+                    if (err.valid()) {
+                        return err;
+                    }
+                }
+            }
+        }
+    }
+    return ErrMsg();
 }
 
 } // namespace
--- src/shdc/generators/bare.h
+++ src/shdc/generators/bare.h
@@ -6,6 +6,8 @@ namespace shdc::gen {
 class BareGenerator: public Generator {
 public:
     virtual ErrMsg generate(const GenInput& gen);
+private:
+    ErrMsg gen_shader_sources_and_blobs(const GenInput& gen, Slang::Enum slang);
 };
 
 } // namespace
