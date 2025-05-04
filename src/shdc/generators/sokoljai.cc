09c7c1bc7642f9507f5ce841d1f5c133231bae20
Katajisto <t@ktj.st>
2025-05-04 21:12:21 +0300

    fix jai struct alignment

--- src/shdc/generators/sokoljai.cc
+++ src/shdc/generators/sokoljai.cc
@@ -177,10 +177,11 @@ void SokolJaiGenerator::gen_struct_interior_decl_std430(const GenInput& gen, con
     if (cur_offset < pad_to_size) {
         l("_: [{}]u8;\n", pad_to_size - cur_offset);
     }
+    l("_: void #align {};\n", struc.align);
 }
 
 void SokolJaiGenerator::gen_storage_buffer_decl(const GenInput& gen, const Type& struc) {
-    l_open("{} :: struct #align {} {{\n", struct_name(struc.struct_typename), struc.align);
+    l_open("{} :: struct {{\n", struct_name(struc.struct_typename));
     gen_struct_interior_decl_std430(gen, struc, struc.size);
     l_close("}}\n");
 }
