32aa5055cfd7b2b812600df1e0d09eff423261a4
Andre Weissflog <floooh@gmail.com>
2026-03-07 16:21:05 +0100

    fibs.ts: add fibs-extras/windows.ts import

--- fibs.ts
+++ fibs.ts
@@ -4,7 +4,7 @@ export function configure(c: Configurer): void {
     c.addImport({
         name: 'extras',
         url: 'https://github.com/floooh/fibs-extras',
-        files: ['vscode.ts', 'macos.ts'],
+        files: ['vscode.ts', 'macos.ts', 'windows.ts'],
     });
 
     // add explicit macos x86_64 vs arm64 build configs
